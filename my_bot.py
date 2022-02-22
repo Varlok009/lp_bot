8210741f3750c75ef289c6cc99475aa9e2e44d86
Никита Кучин <varlok009@mail.ru>
2022-02-22 20:05:20 +0300

    comleate 1 and 2 work

--- my_bot.py
+++ my_bot.py
@@ -10,7 +10,9 @@ from clean_string import get_clean_string
 from difflib import get_close_matches
 
 
-planets = [planet[2] for planet in ephem._libastro.builtin_planets() if planet[1] == 'Planet']
+planets = [
+    planet[2] for planet in ephem._libastro.builtin_planets()
+    if planet[1] == 'Planet']
 
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
@@ -51,7 +53,7 @@ def get_correct_planet_name(user_planet_name: str, planets: list[str]) -> str:
 
 
 def handle_planet(update: telegram.Update, context) -> None:
-    if len(update.message.text.split(' ')) == 1:    
+    if len(update.message.text.split(' ')) == 1:
         update.message.reply_text('Нou must to use /planet with a name planet')
     else:
         planet = get_correct_planet_name(update.message.text.split()[1], planets)
@@ -65,17 +67,54 @@ def handle_planet(update: telegram.Update, context) -> None:
 
 def get_number_words(user_text: str) -> str:
     """
-
+    Возвращает количество слов в переданной строке,
+    исключая символы и цифры
     """
-    pass
+    if not user_text:
+        return 'You must enter at least one word'
+
+    if ' ' not in user_text:
+        return 'Your message contain 1 word'
+
+    count_words = len(user_text.split())
+    return f'Your message contain {count_words} words'
 
 
 def handle_wordcount(update: telegram.Update, context) -> None:
     """
-
+    Отправляет пользовотелю в чат - сколько слов содержит сообщение
+    с командой /wordcount
     """
-    answer_to_user = get_number_words(update.message.text)
+    user_text = get_clean_string(update.message.text)
+    if 'wordcount' in user_text:
+        user_text = user_text.replace('wordcount', '')
 
+    answer_to_user = get_number_words(user_text)
+    update.message.reply_text(answer_to_user)
+
+
+def get_date_next_fool_moon() -> str:
+    """
+    """
+    date_next_full_moon = ephem.next_full_moon(dt.today())
+    date_next_full_moon = dt.strptime(
+                                    str(date_next_full_moon),
+                                    '%Y/%m/%d %H:%M:%S'
+                                    )
+    date_next_full_moon = date_next_full_moon.strftime('%d.%m.%Y %H:%M:%S')
+
+    return date_next_full_moon
+
+
+def handle_next_fool_moon(update: telegram.Update, context) -> None:
+    """
+    Отправляют пользователю в чат - дату следующего полнолуния
+    """
+    # date_next_full_moon = str(get_date_next_fool_moon()).split()
+    date_next_full_moon = get_date_next_fool_moon().split()
+    update.message.reply_text('The next full moon will be {} at {}'.format(
+        date_next_full_moon[0], date_next_full_moon[1]
+    ))
 
 
 def talk_to_me(update: telegram.Update, context) -> None:
@@ -93,6 +132,7 @@ def main():
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("planet", handle_planet))
     dp.add_handler(CommandHandler("wordcount", handle_wordcount))
+    dp.add_handler(CommandHandler("next_fool_moon", handle_next_fool_moon))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
